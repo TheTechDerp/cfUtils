a1f9fe6ba484906d3f4a8154eee13a17dd268833
Rainbow <tehrainbowguy@gmail.com>
2012-09-24 21:43:09 +0100

    basics

---
+++ plugin/src/com/cobbleforest/cfUtils/MoneyHandler.java
@@ -0,0 +1,32 @@
+package com.cobbleforest.cfUtils;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+/**
+ * User: Benjamin
+ * Date: 24/09/12
+ * Time: 21:41
+ */
+public class MoneyHandler {
+    final Economy economy;
+    final Core core;
+
+    public MoneyHandler(Core core) {
+        this.core = core;
+        economy = setupEconomy();
+        if (economy == null) {
+            core.disable("Economy not found!");
+        }
+
+    }
+
+    private Economy setupEconomy() {
+        RegisteredServiceProvider<Economy> economyProvider = core.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        if (economyProvider != null) {
+            return economyProvider.getProvider();
+        }
+
+        return null;
+    }
+}
